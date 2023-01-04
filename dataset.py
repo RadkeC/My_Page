caea9cdf65afece35f7ec5c014fbf628a05405d8
RadkeC <97386875+RadkeC@users.noreply.github.com>
2023-01-04 20:14:54 +0100

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,57 @@
+main_projects = [
+    {'title': 'Device API',
+     'description': 'Aplikacja do zarządzania i grupowania zbioru urządzeń w sieci. Wykonana przy użyciu fastapi i PostgeSQL. Aplikacja wykonana do użytku własnego w pracy',
+     'additional_info': 'Login: Guest | Hasło: guest',
+     'link': 'https://device-api2.onrender.com',
+     'link_name': 'Wypróbuj Device API'},
+
+    {'title': 'Post Blog',
+     'description': 'Aplikacja do umieszczania artykułów w sieci. Umożliwia komentowanie, likowanie i komunikowanie się z innymi autorami. Wykonana przy użyciu django i PostgeSQL.',
+     'additional_info': 'Wkrótce',
+     'link': 'https://github.com/RadkeC/Post_Blog',
+     'link_name': 'Zobacz w GitHub'}
+]
+
+small_projects = [
+    {'title': 'Generator Znaków',
+     'description': 'Aplikacja do tworzenia graficznej wersji napisów w konsoli.',
+     'additional_info': '',
+     'link': 'https://github.com/RadkeC/Small_projects/tree/main/Sign_generator',
+     'link_name': 'Zobacz w GitHub'},
+
+    {'title': 'Saper',
+     'description': 'Gra w sapera wykonana przy użyciu tkinter.',
+     'additional_info': '',
+     'link': 'https://github.com/RadkeC/Small_projects/tree/main/Saper',
+     'link_name': 'Zobacz w GitHub'},
+
+    {'title': 'Kółko-Krzyżyk',
+     'description': 'Gra w kółko i krzyżyk wykonana przy użyciu tkinter.',
+     'additional_info': '',
+     'link': 'https://github.com/RadkeC/Small_projects/tree/main/K%C3%B3%C5%82ko-krzy%C5%BCyk',
+     'link_name': 'Zobacz w GitHub'},
+
+    {'title': 'Kalkulator',
+     'description': 'Prosty kalkulator wykonany przy użyciu tkinter.',
+     'additional_info': '',
+     'link': 'https://github.com/RadkeC/Small_projects/tree/main/Kalkulator',
+     'link_name': 'Zobacz w GitHub'},
+
+    {'title': 'Elektroniczna kostka',
+     'description': 'Program symulujący kostkę do gry w konsoli.',
+     'additional_info': '',
+     'link': 'https://github.com/RadkeC/Small_projects/tree/main/Dice',
+     'link_name': 'Zobacz w GitHub'},
+
+    {'title': 'BMS Reporter',
+     'description': 'Program wypełniający raport w excelu wykonany przy użyciu openpyxl.',
+     'additional_info': 'Aplikacja wykonana do użytku własnego w pracy',
+     'link': 'https://github.com/RadkeC/BMSReporter',
+     'link_name': 'Zobacz w GitHub'},
+
+    {'title': 'The Alarm Destroyer',
+     'description': 'Program potwierdzający alarmy w wizualizacji na ekranie wykonany przy użyciu pyautogui.',
+     'additional_info': 'Aplikacja wykonana do użytku własnego w pracy',
+     'link': 'https://github.com/RadkeC/TheAlarmDestroyer',
+     'link_name': 'Zobacz w GitHub'}
+]
